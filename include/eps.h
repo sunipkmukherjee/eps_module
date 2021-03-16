3a7ec16e55d10b51ed9c889ffae235a1428629e8
mitbailey99 <mitbailey99@gmail.com>
2021-03-16 14:32:55 -0400

    Updated eps.c, eps.h, and Doxygen
    Added necessary command queue functions and made the while(!done) loop execute commands from the queue. Implemented eps_destroy() and added proper declarations to eps.h. Also included Doxygen comments throughout.

--- include/eps.h
+++ include/eps.h
@@ -1,10 +1,10 @@
 /**
  * @file eps.h
  * @author Mit Bailey (mitbailey99@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-03-13
- *  
+ *
  * @copyright Copyright (c) 2021
  *
  */
@@ -12,33 +12,103 @@
 #ifndef EPS_H
 #define EPS_H
 #include <eps_extern.h>
+#include <stdbool.h>
 
-// Put other defines here.
-
-
+ /**
+  * @brief Node object for the command queue.
+  *
+  */
+typedef struct Node {
+	uint8_t* cmd;
+	node_t* next;
+} node_t;
 
 /**
- * @brief Initializes the devices required to run the electronic power supply.
- * 
- * This function initializes everything necessary for the EPS to operate.
- *  
- * @return int 1 on success, error codes defined in SH_ERRORS on error.
+ * @brief Queue which holds EPS commands for processing.
+ *
  */
+typedef struct CmdQ {
+	node_t* head;
+	node_t* tail;
+	uint8_t size;
+	const uint8_t maxSize; // maxSize = 255
+} cmdq_t;
+
+/**
+ * @brief Adds a command to the queue.
+ *
+ * @return bool True on success.
+ */
+bool eps_cmdq_enqueue(uint8_t value[]);
+
+/**
+ * @brief Returns and removes the next command in the queue.
+ *
+ * @return note_t* The next command in the queue.
+ */
+node_t* eps_cmdq_dequeue();
+
+/**
+ * @brief Executes the next command in the queue.
+ *
+ * @return int 1 on success, -1 on dequeue error
+ */
+int eps_cmdq_execute();
+
+/**
+ * @brief Destroys / frees the entire command queue.
+ *
+ */
+void eps_cmdq_destroy();
+
+/**
+ * @brief Queues an EPS ping command request.
+ *
+ */
+void eps_ping();
+
+/**
+ * @brief Queues an EPS reboot command request.
+ *
+ */
+void eps_reboot();
+
+/**
+ * @brief Queues an EPS latch toggle command request.
+ *
+ * @param lup ID of latchup (0 -> 5V1, 1 -> 5V2, 2 -> 5V3, 3 -> 3V1, 4 -> 3V2, 5 -> 3V3)
+ */
+void eps_latchToggle(eps_lup_idx lup);
+
+/**
+ * @brief Queues an EPS latch set command request.
+ *
+ * @param lup ID of latchup (0 -> 5V1, 1 -> 5V2, 2 -> 5V3, 3 -> 3V1, 4 -> 3V2, 5 -> 3V3)
+ * @param pw 0 for off, 1 for on
+ */
+void eps_latchSet(eps_lup_idx lup, int pw);
+
+ /**
+  * @brief Initializes the devices required to run the electronic power supply.
+  *
+  * This function initializes everything necessary for the EPS to operate.
+  *
+  * @return int 1 on success, error codes defined in SH_ERRORS on error.
+  */
 int eps_init();
 
 /**
  * @brief EPS thread.
- * 
+ *
  * @param tid Pointer to an integer containing the thread ID.
  * @return NULL.
  */
 void *eps_thread(void *tid);
 
 /**
- * @brief TBD.
- * 
- * TBD
- * 
+ * @brief Frees and destroys.
+ *
+ *
  * @return NULL.
  */
 void eps_destroy();
--- src/eps.c
+++ src/eps.c
@@ -1,124 +1,149 @@
 /**
  * @file eps.c
  * @author Mit Bailey (mitbailey99@gmail.com)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2021-03-13
- *  
+ *
  * @copyright Copyright (c) 2021
  *
  */
 
-// TO-DO
-// Set up the command queue.
-// See eps_p31u for how a CMD looks.
-// The CmdQueue should handle all incoming command execution requests.
-
 #include <p31u.h>
 #include <eps.h>
 #include <main.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <pthread.h>
 
+/* Variable allocation for EPS */
+
+/**
+ * @brief The EPS object pointer.
+ *
+ */
 p31u* eps;
 
-/* Possible commands include...
+/**
+ * @brief The command queue.
  *
- * 0
- * Ping
- * static inline int eps_p31u_ping(p31u *dev)
- * 
- * 1
- * Reboot
- * static inline int eps_p31u_reboot(p31u *dev)
- * 
- * 2
- * Toggle Latch
- * static inline int eps_p31u_tgl_lup(p31u *dev, eps_lup_idx lup)
- * 
- * 3
- * Set Latch
- * static inline int eps_p31u_lup_set(p31u *dev, eps_lup_idx lup, int pw)
- * 
  */
+cmdq_t* globalCmdQ;
 
-// A Node of the commandQueue
-struct Node {
-    uint8_t cmd[];
-    struct Node* next;
+/**
+ * @brief Boolean to determine if this is the first pass.
+ *
+ */
+int firstRun = 1;
+
+/**
+ * @brief Condition variable for use with eps_mutex.
+ *
+ */
+pthread_cond_t eps_ready;
+
+/**
+ * @brief Mutex to ensure secure data access.
+ *
+ */
+pthread_mutex_t eps_mutex;
+
+bool eps_cmdq_enqueue(uint8_t value[]) {
+	// Allocate memory for a new node.
+	node_t* newNode = malloc(sizeof(node_t));
+
+	// Check is malloc was successful.
+	if (newNode == NULL) {
+		perror("Unable to allocate memory to enqueue command queue!");
+			return false;
+	}
+
+	// Initialize the values of newNode.
+	newNode->cmd = value;
+	newNode->next = NULL;
+
+	// If the queue->tail already exists, set it's next to the
+	// new node. This adds our node to the end of the list.
+	if (globalCmdQ->tail != NULL) {
+		globalCmdQ->tail->next = newNode;
+	}
+
+	// Regardless if a tail existed or not, we now want to establish
+	// our latest addition to the command queue as the tail item.
+	globalCmdQ->tail = newNode;
+
+	// If the queue->head doesn't already exist, set it to the
+	// new node. This would only occur if the queue was previously
+	// empty, making newNode simultaneously the head and the tail.
+	if (globalCmdQ->head == NULL) {
+		globalCmdQ->head = newNode;
+	}
+
+	// Successful.
+	return true;
 }
 
-// The commandQueue struct.
-// This holds a linked list of all commands queued for execution by the EPS.
-struct CmdQueue {
-    struct Node* head;
-    struct Node* tail;
-    uint8_t size;
-    const uint8_t maxSize = 255;
-} commandQueue;
+node_t* eps_cmdq_dequeue() {
+	// Check if the queue is empty, and return an error if so.
+	if (globalCmdQ->head == NULL) {
+		perror("Cannot dequeue an empty command queue!");
+		return NULL;
+	}
 
-bool enqueue(commandQueue *queue, uint8_t value[]){
-    // Allocate memory for a new node.
-    struct Node* newNode = malloc(sizeof(struct Node));
+	// Set the returnNode to the current command queue's head.
+	node_t* returnNode = globalCmdQ->head;
 
-    // Check is malloc was successful.
-    if (newNode == NULL){
-        perror("Unable to allocate memory to enqueue command queue!")
-        return false;
-    }
+	// Set the command queue's head to the next node, since we
+	// are removing the read node.
+	globalCmdQ->head = globalCmdQ->head->next;
 
-    // Initialize the values of newNode.
-    newNode->value = value;
-    newNode->next = NULL;
+	// If the head is now NULL because ...->next was NULL
+	// (aka there was no next), we should be sure to set our
+	// tail to NULL as well.
+	if (globalCmdQ->head == NULL) {
+		globalCmdQ->tail = NULL;
+	}
 
-    // If the queue->tail already exists, set it's next to the
-    // new node. This adds our node to the end of the list.
-    if (queue->tail != NULL){
-        queue->tail->next = newNode;
-    }
-    
-    // Regardless if a tail existed or not, we now want to establish
-    // our latest addition to the command queue as the tail item.
-    queue->tail = newNode;
-
-    // If the queue->head doesn't already exist, set it to the
-    // new node. This would only occur if the queue was previously
-    // empty, making newNode simultaneously the head and the tail.
-    if (queue->head == NULL){
-        queue->head = newNode;
-    }
-
-    // Successful.
-    return true;
+	// Return the old head node that we've dequeued.
+	return returnNode;
 }
 
-struct Node* dequeue(commandQueue *queue){
-    // Check if the queue is empty, and return an error if so.
-    if (queue-> == NULL){
-        perror("Cannot dequeue an empty command queue!");
-        return -1;
-    }
+// Dequeues the top next command, executes it, and frees the memory.
+int eps_cmdq_execute() {
+	node_t* node = dequeue();
 
-    // Set the returnNode to the current command queue's head.
-    struct Node* returnNode = queue->head;
+	if (node == NULL) {
+		perror("Dequeueing error!");
+		return -1;
+	}
 
-    // Set the command queue's head to the next node, since we
-    // are removing the read node.
-    queue->head = queue->head->next;
+	switch (node->cmd[0]) {
+	case 0: // ping
+		eps_p31u_ping(eps);
+		break;
+	case 1: // reboot
+		eps_p31u_reboot(eps);
+		break;
+	case 2: // toggle latch
+		eps_p31u_tgl_lup(eps, node->cmd[1]);
+		break;
+	case 3: // set latch
+		eps_p31u_lup_set(eps, node->cmd[1], node->cmd[2]);
+		break;
+	default: // error
+		perror("Error: Default case reached in eps_cmdq_execute()!");
+		return -1;
+	}
 
-    // If the head is now NULL because ...->next was NULL
-    // (aka there was no next), we should be sure to set our
-    // tail to NULL as well.
-    if (queue->head == NULL){
-        queue->tail = NULL;
-    }
-
-    // Return the old head node that we've dequeued.
-    return returnNode;
+	free(node);
+	return 1;
 }
 
-// Im not sure if this is needed, we will see.
-// It might just so the same thing as dequeue.
-void executeTopCmd(){
-
+void eps_cmdq_destroy() {
+	while (globalCmdQ->head != NULL) {
+		free(dequeue());
+	}
+	return;
 }
 
 // Adds the command to the command queue.
@@ -126,76 +151,84 @@ void executeTopCmd(){
 // Eventually, these should return an address to a location
 // in memory which contains info on the current status of
 // the command request: Pending, Executed, Deleted.
-void eps_ping(){
-    uint8_t arr[] = {0};
-    enqueue(commandQueue, arr);
+void eps_ping() {
+	uint8_t arr[] = { 0 };
+	enqueue(arr);
 
-    return;
+	return;
 }
 
-void eps_reboot(){
-    uint8_t arr[] = {1};
-    enqueue(commandQueue, arr);
+void eps_reboot() {
+	uint8_t arr[] = { 1 };
+	enqueue(arr);
 
-    return;
+	return;
 }
 
-void eps_latchToggle(uint8_t arg2){
-    uint8_t arr[] = {2, arg2};
-    enqueue(commandQueue, arr);
+void eps_latchToggle(eps_lup_idx lup) {
+	uint8_t arr[] = { 2, lup };
+	enqueue(arr);
 
-    return;
+	return;
 }
 
-void eps_latchSet(uint8_t arg2, uint8_t arg3){
-    uint8_t arr[] = {3, arg2, arg3};
-    enqueue(commandQueue, arr);
+void eps_latchSet(eps_lup_idx lup, int pw) {
+	uint8_t arr[] = { 3, lup, pw };
+	enqueue(globalCmdQ, arr);
 
-    return;
+	return;
 }
 
 // Initializes the EPS and ping-tests it.
 // int eps_onAwake(){}
-int eps_init(){
-//#ifdef EPS_Init
+int eps_init() {
+	//#ifdef EPS_Init
 
-    eps = (p31u*)malloc(sizeof(p31u));
-    commandQueue = (struct CmdQueue*)malloc(sizeof(struct CmdQueue));
+	eps = (p31u*)malloc(sizeof(p31u));
+	globalCmdQ = (cmdq_t*)malloc(sizeof(cmdq_t));
 
-    // Initializes the EPS component while checking if successful.
-    if(eps_p31u_init(eps, 1, 0x1b) <= 0){
-        perror("EPS initialization failed!");
-        return -1;
-    }
+	// Initializes the EPS component while checking if successful.
+	if (eps_p31u_init(eps, 1, 0x1b) <= 0) {
+		perror("EPS initialization failed!");
+		return -1;
+	}
 
-    // If we can't successfully ping the EPS then something has gone wrong.
-    if(eps_p31u_ping(eps) < 0){
-        perror("EPS ping test failed!");
-        return -2;
-    }
+	// If we can't successfully ping the EPS then something has gone wrong.
+	if (eps_p31u_ping(eps) < 0) {
+		perror("EPS ping test failed!");
+		return -2;
+	}
 
-//#endif // EPS_Init
+	//#endif // EPS_Init
 }
 
 // Accepts the Thread ID from its caller (main.c)
 // void *eps_onStart(void *tid){}
-void *eps_thread(void *tid){
+void *eps_thread(void *tid) {
 
-    // 'done' is a global Boolean established in main.h and handled in main.c
-    // It is false when this module is alive and true when this module is being
-    // shut down.
-    // while(moduleAlive){}
-    while(!done){
+	// 'done' is a global Boolean established in main.h and handled in main.c
+	// It is false when this module is alive and true when this module is being
+	// shut down.
+	// while(moduleAlive){}
+	while (!done) {
+		if (firstRun) {
+			printf("EPS: Waiting for release...\n");
+			firstRun = 0;
+		}
 
-    }
+		pthread_cond_wait(&eps_ready, &eps_mutex);
 
-    pthread_exit(NULL);
+		eps_cmdq_execute();
+	}
+
+	pthread_exit(NULL);
 }
 
 // Frees eps memory and destroys the EPS object.
 // void eps_onShutdown(p31u* eps){}
-void eps_destroy(p31u* eps){
-    free(eps);
-    free(commandQueue);
-    eps_p31u_destroy(eps);
+void eps_destroy() {
+	free(eps);
+	eps_cmdq_destroy();
+	free(globalCmdQ);
+	eps_p31u_destroy(eps);
 }
